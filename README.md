2ea8ea7f16d14da8e35f2207d2202ec3b17f2e01
DARK6870 <tsymbalvlad.6870@gmail.com>
2024-02-10 14:10:40 +0200

    PAMAGITI

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # lab4
+<h1>Tsymbal Vlad</h1>
+<br>
+<br>
+<img src="./img/1.png">
+<img src="./img/2.png">
+<img src="./img/3.png">
+<img src="./img/4.png">
+<img src="./img/5.png">
+<img src="./img/6.png">
+<img src="./img/7.png">
---
+++ lab4/lab4/MainWindow.xaml.cs
@@ -0,0 +1,237 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Data;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace lab4
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ShowResult(string text)
+        {
+            result.Visibility = Visibility.Visible;
+            result.Text = text;
+        }
+
+        private void replace_btn_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string from = from_tb.Text;
+                string to = to_tb.Text;
+                string text = text_tb.Text;
+                if (text != "")
+                {
+
+                    text = text.Replace(from, to);
+
+                    ShowResult(text);
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show($"{ex.Message}");
+            }
+        }
+
+        private void fix_btn_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string text = text_tb.Text;
+                if (text != "")
+                {
+                    var replace_up = new Regex((@"(^|\n|(^|\n)(—|-)\s|(\.|!|\?)\s)([A-Z]|[А-Я])"), RegexOptions.IgnoreCase);
+                    text = replace_up.Replace(text, m => m.ToString().ToUpper());
+                    ShowResult(text);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        static bool IsPalindrome(string word)
+        {
+            char[] charArray = word.ToCharArray();
+            Array.Reverse(charArray);
+            string reversedWord = new string(charArray);
+
+            return word.Equals(reversedWord, StringComparison.OrdinalIgnoreCase);
+        }
+        static double EvaluateExpression(string expression)
+        {
+            DataTable table = new DataTable();
+            table.Columns.Add("expression", typeof(string), expression);
+
+            DataRow row = table.NewRow();
+            table.Rows.Add(row);
+
+            return double.Parse((string)row["expression"]);
+        }
+
+        private void palindromes_btn_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string text = text_tb.Text;
+                if (text != "")
+                {
+                    List<string> palindromes = new List<string>();
+                    string[] words = text.Split(new[] { ' ', ',', '.', '!', '?' }, StringSplitOptions.RemoveEmptyEntries);
+
+                    foreach (string word in words)
+                    {
+                        if (IsPalindrome(word))
+                        {
+                            palindromes.Add(word);
+                        }
+                    }
+
+                    string result = string.Join(", ", palindromes);
+                    ShowResult(result);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "error" ,MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void count_btn_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string text = text_tb.Text;
+                if (text != "")
+                {
+                    //string[] words = text.Split(new[] { ' ', ',', '.', '!', '?' });
+                    string[] words = text.Split(' ');
+                    int res = words.Count();
+                    ShowResult(res.ToString() + " word(s)");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void exp_btn_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string text = text_tb.Text;
+                if (text != "")
+                {
+                    double result = EvaluateExpression(text);
+                    ShowResult("Result = " + result.ToString());
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+
+        private void enc_btn_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string text = text_tb.Text;
+                if (text != "")
+                {
+                    string res = EncodeText(text);
+                    ShowResult(res);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void dec_btn_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string text = text_tb.Text;
+                if (text != "")
+                {
+                    string res = DecodeText(text);
+                    ShowResult(res);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        static string EncodeText(string text)
+        {
+            StringBuilder encoded = new StringBuilder();
+
+            for(int i = 0; i < text.Length; i++)
+            {
+                char c = text[i];
+                byte ost = (byte) (i % 3);
+
+                if (c == 'c')
+                {
+                    char encodedChar = (char)(c + ost + 1);
+                    encoded.Append(encodedChar);
+                }
+                else
+                {
+                    encoded.Append(c);
+                }
+            }
+            return encoded.ToString();
+        }
+
+        static string DecodeText(string text)
+        {
+            StringBuilder decoded = new StringBuilder();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                char c = text[i];
+                byte ost = (byte)(i % 3);
+                char check = (char)(c - ost - 1);
+                if (check == 'c')
+                {
+                    char decodedChar = (char)(c - ost - 1);
+                    decoded.Append(decodedChar);
+                }
+                else
+                {
+                    decoded.Append(c);
+                }
+            }
+            return decoded.ToString();
+        }
+    }
+}
